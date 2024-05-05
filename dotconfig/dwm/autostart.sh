ce2c798ca6800d034cd37d54697407ee03d6aa58
steve <3337990267@qq.com>
2024-05-05 13:23:50 +0800

    update dot config

--- dotconfig/dwm/autostart.sh
+++ dotconfig/dwm/autostart.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# xrandr --output HDMI-1 --auto --below eDP-1
+xrandr --output HDMI-1 --brightness 0.8
+xrandr --output HDMI-1-1 --brightness 0.8
+# xrandr --output HDMI-1 --scale 0.8x0.8
+# xinput map-to-output 14 "HDMI-1"
+
 # background
 picom &
-feh --bg-fill /home/steve/.local/share/background/back.jpg
+feh --bg-fill /usr/share/background/background.b9890328.png
 
 # mount disk
 udiskie -ans &
@@ -17,6 +23,11 @@ fcitx5 &
 
 # polkit password agent
 rofi-polkit-agent &
-
-xrandr --output HDMI-1 --brightness 0.4
 setxkbmap -option "caps:super"
+
+sleep 2
+
+# exit or paru -Syu
+st -e sh -c "neofetch && echo -e 'If you need to update the system\nplease enter the password\nthis will execute the {Paru -Syu} instruction' && paru -Syu && zsh"
+# nvidia
+optimus-manager-qt &
